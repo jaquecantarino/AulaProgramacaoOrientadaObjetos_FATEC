d740fc3fda64cf43db2e28cd0e0b1af7ef02af84
jaquecantarino <jaquelinecantarino@outlook.com>
2021-08-06 19:51:48 -0300

    hello world

---
+++ Aula 02/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	
+	public static void main (String[] args) {
+		System.out.println("Hello World! Hello Java!");
+		System.out.printf("%5.2f",3.1789); 
+		//esse %5. indica que arredonda quando o proximo numero passar de 5, logo a exibição será 3,18;
+		//%n também pula linha, igual o \n
+		
+		
+	}
+}
